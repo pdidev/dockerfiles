b54e83450391c3765f6fcf69638f08a888fbb9ba
Karol Sierocinski <ksiero@man.poznan.pl>
2020-07-29 13:26:05 +0200

    Disable ctest for cmake3.10

--- test_docker_image.sh
+++ test_docker_image.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+# args: $1 docker image tag (e.g. centos_cmake3.5)
+#       $2 packaging tool (e.g. yum)
+#       $3 "true" if script should call ctest (must be false for cmake < 3.10)
+
 docker_image=$1
 install_cmd="$2 install -y"
+if ["$3" == "true"];then
+    run_ctest="ctest --output-on-failure"
+else
+    run_ctest="true"
+fi
 
 docker run -u 0 -t $docker_image /bin/bash -c "$install_cmd curl sudo;
                                                sudo -u default curl -Lso pdi.tar.gz https://gitlab.maisondelasimulation.fr/pdidev/pdi/-/archive/master/pdi-master.tar.gz && \
@@ -9,4 +18,4 @@ docker run -u 0 -t $docker_image /bin/bash -c "$install_cmd curl sudo;
                                                cd pdi* && \
                                                cmake -DDIST_PROFILE=Devel . && \
                                                make && \
-                                               ctest --output-on-failure"
+                                               $run_ctest"
