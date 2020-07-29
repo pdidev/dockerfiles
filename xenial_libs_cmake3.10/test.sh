c0575deee42f875ff5d5f8c09000e3995d76d437
Karol Sierocinski <ksiero@man.poznan.pl>
2020-07-29 15:25:15 +0200

    Inner bash string for user call

--- xenial_libs_cmake3.10/test.sh
+++ xenial_libs_cmake3.10/test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 docker run -u 0 -t pdidevel/xenial_libs_cmake3.10 /bin/bash -c "apt-get install -y curl sudo;
-                                            sudo -u default curl -Lso pdi.tar.gz https://gitlab.maisondelasimulation.fr/pdidev/pdi/-/archive/master/pdi-master.tar.gz && \
+                                            sudo -u default /bin/bash -c \"curl -Lso pdi.tar.gz https://gitlab.maisondelasimulation.fr/pdidev/pdi/-/archive/master/pdi-master.tar.gz && \
                                             tar -xf pdi.tar.gz && \
                                             cd pdi* && \
-                                            cmake -DDIST_PROFILE=Devel -DUSE_DEFAULT=SYSTEM . && \
+                                            cmake -DDIST_PROFILE=Devel -DUSE_DEFAULT=SYSTEM -DUSE_Bpp=SYSTEM . && \
                                             make \
-                                            ctest --output-on-failure"
+                                            ctest --output-on-failure\""
