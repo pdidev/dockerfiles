78060096ba2b6d49032dbd78a1a97f8370fdb39b
Karol Sierocinski <ksiero@man.poznan.pl>
2020-07-30 08:49:39 +0200

    Fix typos

--- centos_libs_cmake3.10/test.sh
+++ centos_libs_cmake3.10/test.sh
@@ -5,5 +5,5 @@ docker run -t pdidevel/centos_libs_cmake3.10 /bin/bash -c \
                                             tar -xf pdi.tar.gz && \
                                             cd pdi* && \
                                             cmake -DDIST_PROFILE=Devel -DUSE_DEFAULT=SYSTEM . && \
-                                            cmake && \
+                                            make && \
                                             ctest --output-on-failure"
